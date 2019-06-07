9a07b88ca56c45660776e38e445c77d1beaff785
python240419 <51232248+python240419@users.noreply.github.com>
2019-06-07 09:59:41 +0300

    Add files via upload

---
+++ HW/targil1.py
@@ -0,0 +1,11 @@
+
+a = int(input("Enter 1st number"))
+b = int(input("Enter 2nd number"))
+
+if a > b:
+    print(f'{a} is bigger')
+elif a == b:
+    print("equal")
+else:
+    print(f'{b} is bigger')
+
